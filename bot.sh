943c5b5e3fd8cb544805da07ba7204238253eabd
Dimitar Dimitrov <newbrict@gmail.com>
2013-07-16 23:19:17 +0100

    added what I had done

---
+++ bot.sh
@@ -0,0 +1,27 @@
+# !/bin/bash
+
+. bot.properties
+config=".bot.cfg"
+echo "NICK $nick" > $config 
+echo "USER $user" >> $config
+echo "JOIN #$channel" >> $config
+
+tail -f $config | telnet $server 6667 | while read res; do
+# do things when you see output
+  case "$res" in
+    PING*)
+      echo "$res" | sed "s/PING/PONG/" >> $config 
+    ;;
+# for pings on nick/user
+    *"You have not"*)
+      echo "JOIN #$channel" >> $config
+    ;;
+    *JOIN*)
+      whojoin=$(echo "$res" | sed -r "s/:(.*)\!.*@.*/\1/")
+      echo "PRIVMSG #$channel :Welcome $whojoin :)" >> $config
+    ;;
+    *)
+      echo "$res"
+    ;;
+  esac
+done
