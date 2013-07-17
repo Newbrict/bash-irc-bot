5ab8fd674f42d5cdfeab4ff1cce63563994624fe
Dimitar Dimitrov <newbrict@gmail.com>
2013-07-16 21:17:56 -0400

    visual errors

--- bot.sh
+++ bot.sh
@@ -6,13 +6,14 @@ echo "NICK $nick" > $config
 echo "USER $user" >> $config
 echo "JOIN #$channel" >> $config
 
-tail -f $config | telnet $server 6667 | while read res; do
-# do things when you see output
+tail -f $config | telnet $server 6667 | while read res;
+do
+  # do things when you see output
   case "$res" in
     PING*)
       echo "$res" | sed "s/PING/PONG/" >> $config 
     ;;
-# for pings on nick/user
+    # for pings on nick/user
     *"You have not"*)
       echo "JOIN #$channel" >> $config
     ;;
